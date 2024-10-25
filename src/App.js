4e31b23b8b9971522bc3782c249dfb0e173e0477
Taskeen <taskeen.fa@example.com>
2024-10-25 13:51:32 +0530

    color-gen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Accordian from './components/accordian';
+import Random from './components/colour-gen';
 
 function App() {
   return (
     <div className="App">
-      <Accordian/>
+      {/* <Accordian/> */}
+      <Random/>
     </div>
   );
 }
---
+++ src/components/colour-gen/index.jsx
@@ -0,0 +1,9 @@
+const Random=()=>{
+  return(
+    <div className="container">
+     <button>Generate Random Color</button>
+    </div>
+  )
+}
+
+export default Random;
